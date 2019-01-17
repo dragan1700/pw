22502ebf685e0ba927efd93e376baee582dfc0ba
dragan1700 <nikolic@gmail.com>
2019-01-17 16:50:36 +0100

    Create json-header.php

---
+++ json-header.php
@@ -0,0 +1,14 @@
+<?php namespace ProcessWire;
+header("Content-type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: POST, GET");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+header("Expires: 0");
+header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
+header("Cache-Control: no-store, no-cache, must-revalidate");
+header("Cache-Control: post-check=0, pre-check=0", false);
+header("Pragma: no-cache");
+
+$json = json_encode($final); // $final is some kind of array
+echo $json;
+exit();
