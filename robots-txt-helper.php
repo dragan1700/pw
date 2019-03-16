b7054de6d2e97cf6cadfd13c3e4548e9195d1f14
dragan1700 <nikolic@gmail.com>
2019-03-16 18:54:34 +0100

    Update robots-txt-helper.php

--- robots-txt-helper.php
+++ robots-txt-helper.php
@@ -4,7 +4,8 @@
 
 Use-case: You want to disallow search bots from crawling certain files, e.g. all files under parent page x.
 
-Since PW stores files in site/assets/files/page-id/ and not under the virtual URL domain.com/page/sub-page/pagename/, finding all file-folders manually can be tedious.
+Since PW stores files in site/assets/files/page-id/ and not under the virtual URL 
+domain.com/page/sub-page/pagename/, finding all file-folders manually can be tedious.
 
 This output can be copy and pasted in the site's robots.txt as is. Use in special template or Tracy Debugger console.
 
