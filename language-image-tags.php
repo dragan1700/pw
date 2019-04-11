56d24e06fe176b24c34117fd804e22e38544ee6f
dragan1700 <nikolic@gmail.com>
2019-04-11 13:15:14 +0200

    removed unnecessary debug code

--- language-image-tags.php
+++ language-image-tags.php
@@ -5,30 +5,14 @@
 // https://processwire.com/talk/topic/21246-images-gallery-sorting-order-based-on-language/?tab=comments#comment-183814
 // https://processwire.com/blog/posts/processwire-updates-and-new-field-types/
 
-// assuming you use delayed output strategy...
-$content .= "<div class='gallery'>";
-
 $userlang = $user->language->name;
 
-$content .=  "$userlang <hr>"; // just for debugging
-
-$g = $page->gallery->sort('tags'); // just for debugging: show all, sorted by tag
-$content .=  "all: <br>";
-foreach($g as $pic) {
-    $content .=  "{$pic->tags} {$pic->url}<br>";
-}
-$content .=  "<hr>";
-
 $g = $page->gallery->find("tags=$userlang"); // only user's current language images
-$content .=  "userlang: <br>";
 foreach($g as $pic) {
-    $content .=  "{$pic->tags}  {$pic->url}<br>";
+    $content .=  "<img src='{$pic->url}'>";
 }
 
 $g = $page->gallery->find("tags!=$userlang"); // ... and then, everything else
-$content .=  "NOT userlang: <br>";
 foreach($g as $pic) {
-    $content .=  "{$pic->tags}  {$pic->url}<br>";
+    $content .=  "<img src='{$pic->url}'>";
 }
-
-$content .= "</div>";
